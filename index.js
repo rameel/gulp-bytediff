7ba15d6bc70926e71ffb92ae4a03443bec1e7406
rameel <rameel-b@hotmail.com>
2018-02-23 22:45:25 +0600

    Cleanup and formatting

--- index.js
+++ index.js
@@ -1,52 +1,52 @@
-'use strict';
+const colors = require("ansi-colors");
+const filesize = require("filesize");
+const log = require("fancy-log");
+const mapstream = require("map-stream");
+const path = require("path");
 
-var size  = require('filesize'),
-    colors = require("ansi-colors"),
-    log = require("fancy-log"),
-    path  = require('path'),
-    map   = require('map-stream');
+function defaultFormatter(data) {
+    const saved = data.savings > 0 ? " saved " : " gained ";
+    const color = data.savings > 0 ? "green" : "yellow";
+    const start = filesize(data.startSize);
+    const end = colors[color](filesize(data.endSize));
+    const report = ` (${start} -> ${end})`;
+    const filename = colors.magenta(data.fileName);
 
-function bytediff () {
-    return map(function (file, cb) {
-        // Persist the original size of the file for later
-        file.bytediff = {
-            startSize: file.contents ? file.contents.length : null
-        };
-        cb(null, file);
-    });
+    return filename + saved + filesize(Math.abs(data.savings)) + report;
 }
 
-// Convenience method
-bytediff.start = bytediff;
-
-bytediff.stop = function (formatFn) {
-    return map(function (file, cb) {
-        if (typeof formatFn !== 'function') {
-            formatFn = function (data) {
-                var saved = (data.savings > 0) ? ' saved ' : ' gained ';
-                var color = (data.savings > 0) ? 'green' : 'yellow';
-                var start = size(data.startSize);
-                var end = colors[color](size(data.endSize));
-                var report = ' (' + start + ' -> ' + end + ')';
-                return data.fileName + saved +
-                  size(Math.abs(data.savings)) + report;
+const bytediff = {
+    start: function() {
+        return mapstream(function(file, cb) {
+            // Persist the original size of the file for later
+            file.bytediff = {
+                startSize: file.contents ? file.contents.length : null
             };
-        }
+            cb(null, file);
+        });
+    },
+    stop: function(formatter) {
+        return mapstream(function(file, cb) {
+            if (typeof formatter !== "function") {
+                formatter = defaultFormatter;
+            }
 
-        if (file.bytediff.startSize) {
-            var endSize = file.contents.length;
+            if (file.bytediff.startSize) {
+                const endSize = file.contents.length;
+                const message = formatter({
+                    fileName: path.basename(file.path),
+                    startSize: file.bytediff.startSize,
+                    endSize: endSize,
+                    savings: file.bytediff.startSize - endSize,
+                    percent: endSize / file.bytediff.startSize
+                });
 
-            log(formatFn({
-                fileName: path.basename(file.path),
-                startSize: file.bytediff.startSize,
-                endSize: endSize,
-                savings: file.bytediff.startSize - endSize,
-                percent: (endSize / file.bytediff.startSize)
-            }));
-        }
+                log(message);
+            }
 
-        cb(null, file);
-    });
+            cb(null, file);
+        });
+    }
 };
 
 module.exports = bytediff;
--- test.js
+++ test.js
@@ -1,57 +1,60 @@
 /* global describe, it */
 
-'use strict';
+"use strict";
 
-var bytediff = require('./index'),
-    expect   = require('chai').expect,
-    gulp     = require('gulp'),
-    vinyl    = require("vinyl"),
-    map      = require('map-stream');
+const bytediff = require("./index");
+const expect = require("chai").expect;
+const gulp = require("gulp");
+const map = require("map-stream");
+const vinyl = require("vinyl");
 
-function fixture (contents) {
+function fixture(contents) {
     return new vinyl({
         contents: contents,
         cwd: __dirname,
         base: __dirname,
-        path: __dirname + '/fixture.css'
+        path: __dirname + "/fixture.css"
     });
 }
 
-describe('gulp-bytediff', function () {
-    it('should be able to report the new size of a file', function(cb) {
-        var output = '';
-        process.stdout.write = (function(write) {
-            return function(string) {
-                output = string;
+describe("gulp-bytediff", () => {
+    it("should be able to report the new size of a file", cb => {
+        let output = "";
+        process.stdout.write = (write => {
+            return function(text) {
+                output = text;
                 write.apply(process.stdout, arguments);
             };
         })(process.stdout.write);
-        gulp.src('./index.js')
-            .pipe(bytediff())
-            .pipe(map(function(file, done) {
-                file.contents = new Buffer('minification happened');
+
+        gulp.src("./index.js")
+            .pipe(bytediff.start())
+            .pipe(map((file, done) => {
+                file.contents = new Buffer("minification happened");
                 done(null, file);
             }))
             .pipe(bytediff.stop())
-            .pipe(map(function() {
-                expect(output).to.have.string('21 B');
+            .pipe(map(() => {
+                expect(output).to.have.string("21 B");
                 cb();
             }));
     });
-    it('should store the original size on the file object', function (cb) {
-        var stream = bytediff();
 
-        stream.on('data', function (data) {
+    it("should store the original size on the file object", cb => {
+        const stream = bytediff.start();
+
+        stream.on("data", data => {
             expect(data.bytediff.startSize).to.eql(5);
             cb();
         });
 
-        stream.write(fixture(new Buffer('hello')));
+        stream.write(fixture(new Buffer("hello")));
     });
-    it('should pass null contents through', function (cb) {
-        var stream = bytediff();
 
-        stream.on('data', function (data) {
+    it("should pass null contents through", cb => {
+        const stream = bytediff.start();
+
+        stream.on("data", data => {
             expect(data.contents).to.eql(null);
             cb();
         });
